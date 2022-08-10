cbe92e0566950f1d634c75d482e2064b8f108298
Jorrit Vander Mynsbrugge <44178217+jorritvm@users.noreply.github.com>
2022-08-10 12:48:01 +0200

    Update game.py

--- pset4/game/game.py
+++ pset4/game/game.py
@@ -1,39 +1,31 @@
 import random
 
-# random.seed(123)
-
-
-def is_positive_integer(s):
-    if s.isdigit():
-        if int(s) > 0:
-            return True
-    return False
-
 
 def main():
     while True:
-        level = input("Level: ")
-        if is_positive_integer(level):
+        try:
+            level = int(input("Level: "))
+        except ValueError:
+            continue
+        if level > 0:
             break
-
-    level = int(level)
-    choice = random.randint(a=1, b=level)
+    choice = random.randint(1, level)
 
     while True:
-        guess = input("Guess: ")
-
-        if not is_positive_integer(guess):
+        try:
+            guess = int(input("Guess: "))
+        except ValueError:
             continue
-
-        guess = int(guess)
-
-        if guess > level:
-            print("Too large!")
-        elif guess < level:
-            print("Too small!")
         else:
-            print("Just right!")
-            exit(0)
+            if guess > choice:
+                print("Too large!", end="")
+                continue
+            elif guess < choice:
+                print("Too small!", end="")
+                continue
+            else:
+                print("Just right!", end="")
+                break
 
 
 if __name__ == "__main__":
